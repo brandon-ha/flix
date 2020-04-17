f34023ca4909e75f5e4258734f591eefc59c420c
Brandon Ha <bha3@uci.edu>
2020-04-16 20:09:18 -0700

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Brandon Ha on 4/16/20.
+//  Copyright © 2020 bha3@uci.edu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
